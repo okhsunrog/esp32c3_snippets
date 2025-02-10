cb26484d7962197d28740744c608625b877353a9
okhsunrog <me@okhsunrog.dev>
2025-02-10 22:41:40 +0300

    ble now works

--- src/bin/ble_example.rs
+++ src/bin/ble_example.rs
@@ -1,13 +1,27 @@
 #![no_std]
 #![no_main]
+#![feature(impl_trait_in_assoc_type)]
 
 use defmt::info;
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
 use esp_hal::clock::CpuClock;
 use {defmt_rtt as _, esp_backtrace as _};
+use bt_hci::controller::ExternalController;
+use esp_wifi::ble::controller::BleConnector;
+use esp32c3_snippets::ble;
 
-extern crate alloc;
+// change to 255 if something is not working
+pub const L2CAP_MTU: usize = 251;
+
+#[embassy_executor::task]
+async fn run() {
+    loop {
+        info!("Hello world from embassy using esp-hal-async!");
+        // replace the log with blinking led?
+        Timer::after(Duration::from_millis(2_000)).await;
+    }
+}
 
 #[esp_hal_embassy::main]
 async fn main(spawner: Spawner) {
@@ -24,20 +38,18 @@ async fn main(spawner: Spawner) {
     info!("Embassy initialized!");
 
     let timer1 = esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG0);
-    let _init = esp_wifi::init(
+    let init = esp_wifi::init(
         timer1.timer0,
         esp_hal::rng::Rng::new(peripherals.RNG),
         peripherals.RADIO_CLK,
     )
     .unwrap();
 
-    // TODO: Spawn some tasks
-    let _ = spawner;
+    spawner.spawn(run()).unwrap();
 
-    loop {
-        info!("Hello world!");
-        Timer::after(Duration::from_secs(1)).await;
-    }
-
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/v0.23.1/examples/src/bin
+    let mut bluetooth = peripherals.BT;
+    let connector = BleConnector::new(&init, &mut bluetooth);
+    let controller: ExternalController<_, 20> = ExternalController::new(connector);
+    info!("Starting BLE...");
+    ble::run::<_, { L2CAP_MTU }>(controller).await;
 }
